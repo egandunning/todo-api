d63af85f1b1a9b9be0edcac04cb3841d4be8bec6
egandunning <dunninea@plu.edu>
2018-01-08 12:47:55 -0500

    added get /todos route

--- server/server.js
+++ server/server.js
@@ -12,13 +12,17 @@ let app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-   console.log(req.body);
    let todo = new Todo({
       text: req.body.text
    });
-
    todo.save()
    .then(doc => res.send(doc))
+   .catch(err => res.status(400).send(err));
+});
+
+app.get('/todos', (req, res) => {
+   Todo.find()
+   .then(todos => res.send({todos}))
    .catch(err => res.status(400).send(err));
 });
 
