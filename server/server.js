f1cb2a7c741e28581b2d490f7bd1c75d2dc732fb
egandunning <dunninea@plu.edu>
2018-01-09 10:13:53 -0500

    setup application for heroku

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 let app = express();
 
 //json() returns a function
