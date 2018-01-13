bd16b83fada2c219c838cc623a44e6ea90eab8fb
egandunning <dunninea@plu.edu>
2018-01-13 16:11:51 -0500

    added patch todo route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -56,6 +57,33 @@ app.delete('/todos/:id', (req, res) => {
       return res.status(404).send();
    })
    .catch(err => res.status(400).send(err));
+});
+
+app.patch('/todos/:id', (req, res) => {
+   if(!ObjectID.isValid(req.params.id)) {
+      return res.status(404).send();
+   }
+
+   let body = _.pick(req.body, ['completed', 'text']);
+
+   if(_.isBoolean(body.completed) && body.completed) {
+      body.completedAt = new Date().getTime();
+   } else {
+      body.completed = false;
+      body.completedAt = null;
+   }
+
+   Todo.findByIdAndUpdate(req.params.id, {$set:body}, {new: true})
+   .then(todo => {
+      if(!todo) {
+         return res.status(404).send();
+      }
+
+      res.send({todo});
+   })
+   .catch(err => {
+      res.status(400)
+   })
 });
 
 app.listen(port, () => console.log(`Started on port ${port}`));
