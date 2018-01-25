06f5c9cc11c8254b4a95aca1511d91b9c87812de
egandunning <dunninea@plu.edu>
2018-01-24 21:13:38 -0800

    added logout route

--- server/server.js
+++ server/server.js
@@ -116,6 +116,12 @@ app.post('/users/login', (req, res) => {
    });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+   req.user.removeToken(req.token)
+   .then(() => res.send())
+   .catch(() => res.status(400).send());
+});
+
 app.get('/users/me', authenticate, (req, res) => {
    res.send(req.user);
 });
