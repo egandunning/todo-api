79f06561787a3c083851273605d8de46fa8d90d8
egandunning <dunninea@plu.edu>
2018-03-04 09:50:27 -0600

    changed capitalization in header

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,8 +1,8 @@
 let allowCors = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-    res.header('Access-Control-Allow-Headers', 'x-auth, Content-Type');
-    res.header('Access-Control-Expose-Headers', 'x-auth, Content-Type');
+    res.header('Access-Control-Allow-Headers', 'X-Auth, Content-Type');
+    res.header('Access-Control-Expose-Headers', 'X-Auth, Content-Type');
     next();
 }
 
