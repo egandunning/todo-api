34af1c21d602f2fa6076379e2183a2349bf9d98b
egandunning <dunninea@plu.edu>
2018-03-04 09:42:56 -0600

    expose x-auth header for CORS in middleware

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -2,6 +2,7 @@ let allowCors = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
     res.header('Access-Control-Allow-Headers', 'x-auth, Content-Type');
+    res.header('Access-Control-Expose-Headers', 'x-auth, Content-Type');
     next();
 }
 
