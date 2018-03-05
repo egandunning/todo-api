1856a86e56939837f2395ffb0106ca51651347df
egandunning <dunninea@plu.edu>
2018-03-04 21:01:03 -0600

    CORS troubleshooting - added options to allowed methods

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,6 +1,6 @@
 let allowCors = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'X-Auth, Content-Type');
     res.header('Access-Control-Expose-Headers', 'X-Auth, Content-Type');
     res.header('Access-Control-Request-Headers', 'X-Auth');
