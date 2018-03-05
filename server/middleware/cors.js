9bf9db6a0574a351a5606180a1dd13a6c98f6121
egandunning <dunninea@plu.edu>
2018-03-04 20:46:27 -0600

    allow x-auth header with CORS

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -3,6 +3,7 @@ let allowCors = (req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Auth, Content-Type');
     res.header('Access-Control-Expose-Headers', 'X-Auth, Content-Type');
+    res.header('Access-Control-Headers-Request', 'X-Auth');
     next();
 }
 
